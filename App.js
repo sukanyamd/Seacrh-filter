460894b40c8b6dc7afb89099c8ae2e702fb404f6
sukanyamd <136873174+sukanyamd@users.noreply.github.com>
2023-12-14 19:59:15 -0800

    App.js

--- App.js
+++ App.js
@@ -1,48 +1,8 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import { useState } from 'react';
 
-// const TemperatureComponent=()=>{
-// const[temperature,settemperature]=useState(0);
-// const incrementTemperature = () => {
-//   settemperature(temperature + 1);
-// };
 
-// const decrementTemperature = () => {
-//   settemperature(temperature - 1);
-// };
 
-// const backgroundcolor=()=>{
-//   if(temperature>30){
-//     return 'red';
-//   }
-//   else if(temperature<=30){
-//   return 'blue';
-// }
-// else{
-//   return 'green';
-// }
-// }
 
-// const temperatureStyle = {
-//   backgroundColor: backgroundcolor(),
  
-// };
-
-
-
-
-  // return (
-//     <div style={temperatureStyle}>
-//       <h2><center>{temperature}</center></h2>
-//       <center><button onClick={incrementTemperature}>Increment</button>
-//       <button onClick={decrementTemperature}>Decrement</button></center>
-      
-//     </div>
-//   );
-// }
-
-// export default TemperatureComponent;
 import logo from './logo.svg';
 import './App.css';
 
@@ -50,14 +10,12 @@ import React, { useState } from 'react';
 
 const SearchBar = () => {
   const initialItems = [
-    'Apple',
-    'Banana',
-    'Cherry',
-    'Date',
-    'Grape',
-    'Lemon',
-    'Orange',
-    'Peach',
+  'sukanya',
+   'Anil',
+    'Amritha',
+    'Arun',
+    'Samuel',
+   'San',
   ];
 
   const [items, setItems] = useState(initialItems);
